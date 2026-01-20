fd280457f991f2c6530f8c4c9aa4c38196ac7784
Emir <bermetbermetmurad@gmail.com>
2026-01-20 15:21:12 +0600

    save

--- src/components/Catalog/catalog2/Catalog2.jsx
+++ src/components/Catalog/catalog2/Catalog2.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./catalog2.css";
+import "./Catalog2.css";
 import { Link } from "react-router-dom";
 
 export default function Catalog2() {
